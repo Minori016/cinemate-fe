1521eca98513b0e47251cc506e9c80b0e6ab84ab
Lương Hoàng An <xuhoadon@gmail.com>
2026-07-24 01:21:42 +0700

    fix: import useCallback to resolve ReferenceError

--- src/pages/user/MovieDetailPage.jsx
+++ src/pages/user/MovieDetailPage.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef, useMemo } from 'react'
+import { useState, useEffect, useRef, useMemo, useCallback } from 'react'
 import { useParams, Link, useNavigate, useSearchParams, useLocation } from 'react-router-dom'
 import { movieService } from '../../services/movieService'
 import { bookingService } from '../../services/bookingService'
